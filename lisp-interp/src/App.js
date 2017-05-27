ca722d33018ba6c8e728ede392417fbaf06a7a47
dbkvn <kevinmartinucr@gmail.com>
2017-05-27 13:42:57 -0700

    Reorganized the page header. Changed page title.

--- lisp-interp/src/App.js
+++ lisp-interp/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { PageHeader } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,12 +8,14 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2 classname="App-title">Welcome to my LISP Interpreter!</h2>
+          <div className="App-logo-wrapper">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div className="App-intro">
+            <p>This is a project aiming to implemenent a simple client-side LISP interpreter using React.</p>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
