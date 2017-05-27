ef5cac2d1f4459f8ef899be063ac82b77f544530
dbkvn <kevinmartinucr@gmail.com>
2017-05-27 12:27:00 -0700

    Played around with this to check that I could correctly create a React Bootstrap element. Buttons could be created correctly. Reset the App.js file. This is connected now.

--- lisp-interp/src/index.js
+++ lisp-interp/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import 'bootstrap-css-only';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
