e1a5021ade0dc35baeb58059b7f358376a529da9
dbkvn <kevinmartinucr@gmail.com>
2017-05-28 11:28:08 -0700

    Added a new C# class library, and a new test project. These are both blank, and will require some config work. Link the test project to the class library, this is done by adding an <itemgroup> tag to the csproj file. There are tutorials of this online, MSTest isn't too complicated, so this should only be a few steps. Until I'm back online, I will work on the class project, and use print statements to debug because I don't want to get bogged down in configuration stuff while I am trying to hash out the logic.

---
+++ lisp-c-sharp-logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace lisp_c_sharp_logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ test-lisp-c-sharp-logic/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test_lisp_c_sharp_logic
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
