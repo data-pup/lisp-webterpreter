99ab5589a60c8a57f0b6247e4fe33e00a39b0805
dbkvn <kevinmartinucr@gmail.com>
2017-05-31 00:07:26 -0700

    Moved the helper classes into a subdirectory, added a new file which will expose a simpler API.

---
+++ InterpreterCore/Classes/InputParser.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace InterpreterCore
+{
+    public class InputParser
+    {
+        /// <summary>
+        /// This class is a wrapper around the helper methods used to parse
+        /// a string input from the interpreter into a list of arguments
+        /// for runtime. Note that this does not implement an abstract syntax
+        /// tree. The list returned is a flat list of strings, with superfluous
+        /// characters removed.
+        /// </summary>
+        public static List<String> ParseExpression(string expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
