7e677c690547a719102acefe97edc69b2f784abd
dbkvn <kevinmartinucr@gmail.com>
2017-06-02 21:16:47 -0700

    Input parsing test bench is implemented, and first test case passes. This is abstracted into a helper method so all one needs to do is instantiate a dictionary of inputs and expected outputs, and pass it to the helper method. The helper runs all tests sequentially, and asserts that the output lists match.

--- InterpreterCore.Tests/InputParser.test.cs
+++ InterpreterCore.Tests/InputParser.test.cs
@@ -21,6 +21,33 @@ namespace InterpreterCore.Tests
             var testObject = new InputParser();
         }
 
+        [TestMethod]
+        public void TestInputParserCanParseSimpleAdditionExpressions()
+        {
+            var simpleAdditionExpressions = new Dictionary<string,List<string>>()
+            {
+                { "(+ 1 1)", new List<string> {"(", "+", "1", "1", ")"} }
+            };
+            TestAllExpressionsParseCorrectly(simpleAdditionExpressions);
+        }
+
+        /// <summary>
+        /// This is a private helper method that will read a dictionary of test
+        /// inputs and expected outputs. For each item in the dictionary, the
+        /// actual results of the InputParser.ParseExpressionIntoList method
+        /// will be checked using the CheckSyntaxTokenListsAreEqual method.
+        /// </summary>
+        private void TestAllExpressionsParseCorrectly(Dictionary<string,List<string>> expressionsAndResults)
+        {
+            foreach(var currentTestCase in expressionsAndResults)
+            {
+                string inputExpression = currentTestCase.Key;
+                List<string> expectedSyntaxTokenList = currentTestCase.Value;
+                List<string> actualSyntaxTokenList = InputParser.ParseExpressionIntoList(inputExpression);
+                Assert.IsTrue(CheckSyntaxTokenListsAreEqual(expectedSyntaxTokenList, actualSyntaxTokenList));
+            }
+        }
+
         /// <summary>
         /// This is a private helper method that will examine two syntax
         /// token lists, assert that they have equal lengths, and then
@@ -28,11 +55,22 @@ namespace InterpreterCore.Tests
         /// 'verbosity' parameter is set to true, the two token lists
         /// will be printed in full before comparing.
         /// </summary>
-        private void SyntaxTokenListsAreEqual()
+        private bool CheckSyntaxTokenListsAreEqual(List<string> expectedSyntaxTokenList,
+                                                   List<string> actualSyntaxTokenList)
         {
-            //
+            if(expectedSyntaxTokenList.Count != actualSyntaxTokenList.Count)
+            {
+                return false;
+            }
+            for(int currentTokenIndex = 0; currentTokenIndex < expectedSyntaxTokenList.Count;
+                                           currentTokenIndex++)
+            {
+                string expectedToken = expectedSyntaxTokenList[currentTokenIndex];
+                string actualToken = actualSyntaxTokenList[currentTokenIndex];
+                if(expectedToken != actualToken)
+                    return false;
+            }
+            return true;
         }
-
-
     }
 }
