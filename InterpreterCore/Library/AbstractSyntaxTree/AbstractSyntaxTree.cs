9f4cae016ec4c6ed5a18563e612737a9b7d8dc30
dbkvn <kevinmartinucr@gmail.com>
2017-06-04 02:33:52 -0700

    Commented out the (string[]) constructor, that will be completed tomorrow.

--- InterpreterCore/Library/AbstractSyntaxTree/AbstractSyntaxTree.cs
+++ InterpreterCore/Library/AbstractSyntaxTree/AbstractSyntaxTree.cs
@@ -13,44 +13,44 @@ namespace InterpreterCore.AbstractSyntaxTree
             _root = new LISPAbstractSyntaxTreeNode();
         }
 
-        public LISPAbstractSyntaxTree(string[] rawTokens)
-        {
-            if(rawTokens.Length == 0)
-            {
-                return;
-            }
-            int numberOfTokens = rawTokens.Length;
-            int nestingIndex = 0;
-            var currentNode = _root;
-            for(int currentTokenIndex = 0; currentTokenIndex < numberOfTokens; currentTokenIndex++)
-            {
-                string currentToken = rawTokens[currentTokenIndex];
-                if(currentToken == "(")
-                {   // Open a new nested list, and increase the nesting level.
-                    // TODO Add token
-                    // 
-                    nestingIndex++;
-                    continue;
-                }
-                else if(currentToken == ")")
-                {   // Decrease the nesting level, check for miscmatched parentheses.
-                    nestingIndex--;
-                    if(nestingIndex < 0)
-                    {   // Parentheses do not match if nesting level ever drops below 0.
-                        throw new InvalidProgramException("Syntax error: Parentheses do not match.");
-                    }
-                    continue;
-                }
-                else
-                {   // 
-                    // TODO Add token
-                }
-            }
-            if(nestingIndex != 0)
-            {   // Check that the nesting index is 0 after reaching the end of the token list.
-                throw new InvalidProgramException("Syntax error: Parentheses do not match.");
-            }
-        }
+        // public LISPAbstractSyntaxTree(string[] rawTokens)
+        // {
+        //     if(rawTokens.Length == 0)
+        //     {
+        //         return;
+        //     }
+        //     int numberOfTokens = rawTokens.Length;
+        //     int nestingIndex = 0;
+        //     var currentNode = _root;
+        //     for(int currentTokenIndex = 0; currentTokenIndex < numberOfTokens; currentTokenIndex++)
+        //     {
+        //         string currentToken = rawTokens[currentTokenIndex];
+        //         if(currentToken == "(")
+        //         {   // Open a new nested list, and increase the nesting level.
+        //             // TODO Add token
+        //             // 
+        //             nestingIndex++;
+        //             continue;
+        //         }
+        //         else if(currentToken == ")")
+        //         {   // Decrease the nesting level, check for miscmatched parentheses.
+        //             nestingIndex--;
+        //             if(nestingIndex < 0)
+        //             {   // Parentheses do not match if nesting level ever drops below 0.
+        //                 throw new InvalidProgramException("Syntax error: Parentheses do not match.");
+        //             }
+        //             continue;
+        //         }
+        //         else
+        //         {   // 
+        //             // TODO Add token
+        //         }
+        //     }
+        //     if(nestingIndex != 0)
+        //     {   // Check that the nesting index is 0 after reaching the end of the token list.
+        //         throw new InvalidProgramException("Syntax error: Parentheses do not match.");
+        //     }
+        // }
 
         public LISPAbstractSyntaxTreeNode Root
         {
